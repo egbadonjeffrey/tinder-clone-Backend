095d63ec17c1b741fd3a9c460332d03112dbcba7
egbadonjeffrey <egbadonjeffrey@gmail.com>
2023-11-27 14:39:47 +0000

    fix merge issue

--- index.js
+++ index.js
@@ -8,24 +8,6 @@ require("dotenv").config();
 
 const uri = process.env.URI;
 
-const { v4: uuidv4 } = require("uuid");
-const jwt = require("jsonwebtoken");
-const cors = require("cors");
-const bcrypt = require("bcrypt");
-const { MongoClient } = require("mongodb");
-const client = new MongoClient(uri);
-
-const express = require("express");
-const { MongoClient } = require("mongodb");
-const { v4: uuidv4 } = require("uuid");
-const jwt = require("jsonwebtoken");
-const cors = require("cors");
-const bcrypt = require("bcrypt");
-require("dotenv").config();
-
-const uri = process.env.URI;
-
-
 
 // Default
 app.get("/", (req, res) => {
